ca30a5fdd7fb900d352a348177c378aa72a7249e
HMaRio <mario@Marios-iMac.local>
2015-12-15 16:58:44 +0100

    data
    daten uploaden

---
+++ Assets/Rotate.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+	public float rotationspeed = 100.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.S)) {
+			transform.Rotate (0, 0, rotationspeed * Time.deltaTime);
+				}
+		if (Input.GetKey (KeyCode.W)) {
+			transform.Rotate (0,0, -rotationspeed * Time.deltaTime);
+		}
+
+	}
+}
---
+++ Assets/movescript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movescript : MonoBehaviour {
+	public float speed = 1f;
+
+	// Use this for initialization
+	void Start () {
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		transform.Translate (Input.GetAxis ("Horizontal") * Time.deltaTime, 0.0f, 0.0f);
+	
+	}
+}
